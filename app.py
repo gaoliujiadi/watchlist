49b0b2b4729eb2daa69dd395e9f8af865879e328
gaoliujiadi <gaoliujd@163.com>
2020-03-23 21:16:34 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ else:
     prefix = 'sqlite:////'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path,'data.db')
-app.config['SWLALCHEMY_TRACK_MODIFICATIONS'] = False#关闭对模型修改的监控
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False#关闭对模型修改的监控
 #在扩展类实例化前加载配置
 db = SQLAlchemy(app)
 
@@ -30,6 +30,15 @@ class Movie(db.Model):#表名将会是movie
 
 
 import click
+
+@app.cli.command()  # 注册为命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
 
 @app.cli.command()
 def forge():
@@ -71,23 +80,19 @@ def test_url_for():
 	print(url_for('test_url_for'))
 	print(url_for('test_url_for',num=2))
 	return 'Test page'
-'''
-name = 'Grey Li'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
-'''
+
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(usr=user)# 需要返回字典，等同于return {'user': user}
+
 @app.route('/')
 def index():
     user = User.query.first()
     movies = Movie.query.all()
-	return render_template('index.html',uesr = user,movies=movies)
+    return render_template('index.html',user=user,movies=movies)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    user = User.query.first()
+    return render_template('404.html',user=user),404
